88e223bae70cb321727bc70c82a6ab105b960076
shagu <ericmauser@googlemail.com>
2021-06-14 18:36:27 +0200

    index: add all tables that include data

--- index.js
+++ index.js
@@ -18,28 +18,53 @@ const characters = {
   //    Check the ignored values: exploredZones, equipmentCache, ammoId, knownTitles, actionBars
   //    The characters.data table is required by OregonCore but does not exist in CMaNGOS
   //
+  // List of all CMaNGOS tables with contents:
+
+  // 'account_data'
+  // 'account_instances_entered'
+  // 'character_account_data'
+  'character_action': true,
+  // 'character_aura'
+  // 'character_db_version'
   'character_homebind': true,
   'character_inventory': true,
   'character_pet': { ignore: { 'xpForNextLoyalty': true }},
   'character_queststatus': true,
+  // 'character_queststatus_monthly'
   'character_reputation': true,
+  'character_skills': true,
+  // 'character_social'
   'character_spell': true,
+  // 'character_spell_cooldown'
+  // 'character_tutorial'
   'characters': { ignore: { 'exploredZones': true, 'equipmentCache': true, 'ammoId': true, 'knownTitles': true, 'actionBars': true }, replace: { 'power1': 'powerMana', 'power2': 'powerRage', 'power3': 'powerFocus', 'power4': 'powerEnergy', 'power5': 'powerHappiness' }},
+  // 'creature_respawn'
+  // 'event_group_chosen'
+  // 'game_event_status'
+  // 'gameobject_respawn'
+  // 'gm_tickets'
   'guild': { fill: { 'createdate': 0 }},
+  //'guild_bank_eventlog': { replace: { 'EventType': 'LogEntry'}}, -- see TODO
   'guild_bank_item': true,
-  'guild_member': true,
-  'guild_rank': true,
-  'item_instance': true,
-  'mail': true,
-  'mail_items': true,
-  'item_text': true,
-  'pet_spell': true,
   'guild_bank_right': true,
   'guild_bank_tab': true,
-  'character_action': true,
-  'character_skills': true,
-  //'guild_bank_eventlog': { replace: { 'EventType': 'LogEntry'}}, -- see TODO
-  'guild_eventlog': true
+  'guild_eventlog': true,
+  'guild_member': true,
+  'guild_rank': true,
+  // 'instance_reset'
+  'item_instance': true,
+  // 'item_instance_backup_pre_data_field_drop'
+  // 'item_loot'
+  'item_text': true,
+  'mail': true,
+  'mail_items': true,
+  // 'pet_aura'
+  'pet_spell': true,
+  // 'pet_spell_cooldown'
+  // 'petition_sign'
+  // 'saved_variables'
+  // 'world'
+  // 'world_state'
 }
 
 const mariadb = require('mariadb')
