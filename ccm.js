8d338283eaead96419569ce7c08d3ba058366eb0
shagu <ericmauser@googlemail.com>
2021-06-17 16:30:24 +0200

    ccm: rewrite of the the mysql handle logic

--- ccm.js
+++ ccm.js
@@ -12,77 +12,48 @@ const mysql_pass = "mangos"
 const mariadb = require('mariadb')
 const mappings = require('./mappings.js')
 
+const pool = mariadb.createPool({host: mysql_host, user: mysql_user, password: mysql_pass, connectionLimit: 1024});
+
 // try to guess the best `data` values for oregon's characters table
 async function oregon_characters_data(source, dest) {
-  const pool = mariadb.createPool({host: mysql_host, user: mysql_user, password: mysql_pass, connectionLimit: 50});
-
   let conn;
   try {
     conn = await pool.getConnection();
     let rows = await conn.query("SELECT * from " + dest + ".characters")
-    rows.forEach(async function(row) {
+    for await (row of rows) {
       let bitmask = mappings.data_bitmasks[`${row.race} ${row.class} ${row.gender}`]
       let model = mappings.models[`${row.race} ${row.gender}`]
       let estimate = `${row.guid} 0 25 0 1065353216 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 ${row.health} ${row.powerMana} ${row.powerRage} ${row.powerFocus} ${row.powerEnergy} ${row.powerHappiness} ${row.health} 0 1000 0 100 0 ${row.level} ${row.race} ${bitmask}${"".padStart(115*2," 0")} ${model} ${model}${"".padStart(85*2," 0")} ${row.playerBytes} ${row.playerBytes2}${"".padStart(1351*2," 0")}`
       let query = `UPDATE ${dest}.characters SET data='${estimate}' WHERE guid IN (${row.guid});`
-      let res = await conn.query(query)
-    });
+      await conn.query(query)
+    };
   } catch (err) {
     throw err
   } finally {
-    if (conn) conn.release()
-    return pool
+    if (conn) await conn.release()
   }
 }
 
 // add all languages that should be available indicated by skills
 async function oregon_character_spell_languages(source, dest) {
-  const pool = mariadb.createPool({host: mysql_host, user: mysql_user, password: mysql_pass, connectionLimit: 50});
-
   let conn;
   try {
     conn = await pool.getConnection();
     let rows = await conn.query("SELECT * from " + dest + ".character_skills")
-    rows.forEach(async function(row) {
-      if(mappings.language[row.skill]) {
-        let query = "REPLACE INTO " + dest + `.character_spell (guid, spell, active, disabled) VALUES (${row.guid}, ${mappings.language[row.skill]}, 1, 0)`
-        let res = await conn.query(query)
+    for await (let row of rows) {
+      if(mappings.language[row.skill] !== undefined) {
+        await conn.query(`INSERT IGNORE INTO ${dest}.character_spell (guid, spell, active, disabled) VALUES (${row.guid}, ${mappings.language[row.skill]}, 1, 0)`)
       }
-    });
+    }
   } catch (err) {
     throw err
   } finally {
-    if (conn) conn.release()
-    return pool
-  }
-}
-
-// add all equipment spells that should be available indicated by skills
-async function oregon_character_spell_equipment(source, dest) {
-  const pool = mariadb.createPool({host: mysql_host, user: mysql_user, password: mysql_pass, connectionLimit: 50});
-
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    let rows = await conn.query("SELECT * from " + dest + ".character_skills")
-    rows.forEach(async function(row) {
-      if(mappings.equipment[row.skill]) {
-        let query = "REPLACE INTO " + dest + `.character_spell (guid, spell, active, disabled) VALUES (${row.guid}, ${mappings.equipment[row.skill]}, 1, 0)`
-        let res = await conn.query(query)
-      }
-    });
-  } catch (err) {
-    throw err
-  } finally {
-    if (conn) conn.release()
-    return pool
+    if (conn) await conn.release()
   }
 }
 
 // convert cmangos pet action identifiers to oregon format
 async function oregon_character_pet_abdata(source, dest) {
-  const pool = mariadb.createPool({host: mysql_host, user: mysql_user, password: mysql_pass, connectionLimit: 50});
-
   let conn;
   try {
     conn = await pool.getConnection();
@@ -91,18 +62,18 @@ async function oregon_character_pet_abdata(source, dest) {
 
     // fix character_pet abdata values
     let rows = await conn.query("SELECT id, abdata from " + dest + ".character_pet")
-    rows.forEach(async function(row) {
+
+    for await (row of rows) {
       let abdata = row.abdata
       abdata = abdata.replace(/7 2 7 1 7 0/g, "1792 2 1792 1 1792 0") // attack/follow/stay identifier
       abdata = abdata.replace(/6 2 6 1 6 0/g, "1536 2 1536 1 1536 0") // aggressive/defensive/passive identifier
       abdata = abdata.replace(/ 193 /g, " 33024 ") // actionbar identifier
       let res = await conn.query(`UPDATE ${dest}.character_pet SET abdata='${abdata}' WHERE id IN (${row.id});`)
-    });
+    };
   } catch (err) {
     throw err
   } finally {
-    if (conn) conn.release()
-    return pool
+    if (conn) await conn.release()
   }
 }
 
@@ -131,7 +102,7 @@ migrations["cmangos-to-oregon-characters"] = {
   'character_reputation': true,
   'character_skills': true,
   'character_social': true,
-  'character_spell': { finalize: { 'languages': oregon_character_spell_languages, 'equipment': oregon_character_spell_equipment }},
+  'character_spell': { finalize: { 'languages': oregon_character_spell_languages }},
   'character_spell_cooldown': { rename: { 'SpellId': 'spell', 'ItemId': 'item', 'SpellExpireTime': 'time' }, ignore: { 'Category': true, 'CategoryExpireTime': true } },
   'character_tutorial': true,
   'characters': { ignore: { 'exploredZones': true, 'equipmentCache': true, 'ammoId': true, 'knownTitles': true, 'actionBars': true }, rename: { 'power1': 'powerMana', 'power2': 'powerRage', 'power3': 'powerFocus', 'power4': 'powerEnergy', 'power5': 'powerHappiness' }, finalize: { 'data': oregon_characters_data }},
@@ -151,7 +122,7 @@ migrations["cmangos-to-oregon-characters"] = {
   // 'instance_reset'
   'item_instance': true,
   // 'item_instance_backup_pre_data_field_drop'
-  'item_loot': true,
+  // 'item_loot': true,
   'item_text': true,
   'mail': true,
   'mail_items': true,
@@ -173,11 +144,10 @@ let dbcount = 0
 let finalize_queue = { }
 async function migrate(source, dest, table, modifiers, basetable) {
   console.log("Processing: " + table)
-  const pool = mariadb.createPool({host: mysql_host, user: mysql_user, password: mysql_pass, connectionLimit: 50});
 
   if(modifiers.finalize) {
     for (const [name, func] of Object.entries(modifiers.finalize)) {
-      console.log(`Adding "${table}.${name}" to finalize queue`)
+      console.log(`-> Add "${table}.${name}" to finalize queue`)
       finalize_queue[`${table}.${name}`] = func
     }
   }
@@ -185,9 +155,9 @@ async function migrate(source, dest, table, modifiers, basetable) {
   let conn;
   try {
     conn = await pool.getConnection();
-    let trunc = await conn.query("TRUNCATE " + dest + ".`" + table + "`;");
+    await conn.query("TRUNCATE " + dest + ".`" + table + "`;")
     let rows = await conn.query("SELECT * from " + source + "." + table)
-    rows.forEach(async function(row) {
+    for await (row of rows) {
       let query = "INSERT INTO " + dest + "." + table + " ("
       let first = true
 
@@ -230,52 +200,53 @@ async function migrate(source, dest, table, modifiers, basetable) {
         query = 'SELECT 1 as val'
       }
 
-      threadcount++
-      let res = await conn.query(query).then(() => {
-        threadcount--
-
-        // Wait for all write threads to start finalizing
-        if ((threadcount === 0) && (dbcount >= Object.keys(basetable).length)){
-          for (const [name, func] of Object.entries(finalize_queue)) {
-            console.log(`Finalizing: ${name}`)
-            func(source, dest).then((pool) => { if(pool) pool.end() })
-          }
-        }
-      })
-    });
+      await conn.query(query)
+    }
   } catch (err) {
     throw err
   } finally {
-    if (conn) conn.release()
-    return pool
+    if (conn) await conn.release()
   }
 }
 
-// check for all arguments to exist
-if((script === undefined) || (sourceDB === undefined) || (destinationDB === undefined)){
-  console.log("USAGE:")
-  console.log("  node ccm.js <script> <source> <destination>\n")
+(async function() {
+  // check for all arguments to exist
+  if((script === undefined) || (sourceDB === undefined) || (destinationDB === undefined)){
+    console.log("USAGE:")
+    console.log("  node ccm.js <script> <source> <destination>\n")
 
-  console.log("EXAMPLE:")
-  console.log("  node ccm.js cmangos-to-oregon-characters cmangos_chars characters\n")
+    console.log("EXAMPLE:")
+    console.log("  node ccm.js cmangos-to-oregon-characters cmangos_chars characters\n")
 
-  console.log(`SCRIPTS:`)
-  for (const [key, value] of Object.entries(migrations)) {
-    console.log(`  - ${key}`)
+    console.log(`SCRIPTS:`)
+    for (const [key, value] of Object.entries(migrations)) {
+      console.log(`  - ${key}`)
+    }
+
+    process.exit(1)
+  } else if (migrations[script] === undefined) {
+    console.log(`ERROR: Could not find script: ${script}!\n`)
+
+    console.log(`SCRIPTS:`)
+    for (const [key, value] of Object.entries(migrations)) {
+      console.log(`  - ${key}`)
+    }
+
+    process.exit(1)
+  } else {
+    // run core migrations
+    for await (const [key, value] of Object.entries(migrations[script])) {
+      await migrate(sourceDB, destinationDB, key, value, migrations[script])
+    }
+
+    // run finalize functions
+    for await (const [name, func] of Object.entries(finalize_queue)) {
+      console.log(`Finalizing: ${name}`)
+      await func(sourceDB, destinationDB)
+    }
+
+    // close pool connection
+    if (pool) await pool.end()
   }
+})();
 
-  process.exit(1)
-} else if (migrations[script] === undefined) {
-  console.log(`ERROR: Could not find script: ${script}!\n`)
-
-  console.log(`SCRIPTS:`)
-  for (const [key, value] of Object.entries(migrations)) {
-    console.log(`  - ${key}`)
-  }
-
-  process.exit(1)
-} else {
-  for (const [key, value] of Object.entries(migrations[script])) {
-    migrate(sourceDB, destinationDB, key, value, migrations[script]).then((pool) => { pool.end(); dbcount++ })
-  }
-}
